3f0fbb89f8336372efa5413b38f0b819cc1f9e26
Lucjan Wiśnicz <luqapo@gmail.com>
2018-12-04 20:56:30 +0100

    comments and corections

--- auth/AuthController.js
+++ auth/AuthController.js
@@ -1,36 +1,68 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const bcrypt = require ('bcryptjs');
+const bcrypt = require('bcryptjs');
 
 const User = require('../models/User');
 
 const router = express.Router();
-router.use(bodyParser.urlencoded({ extended: false }));
+router.use(bodyParser.urlencoded({
+    extended: false
+}));
 router.use(bodyParser.json());
 
-router.post('/register', (req,res) => {
-    const hashedPassword = bcrypt.hashSync(req.body.password, 8);
+// Api endpoint for register new user
 
-    User.create({
-        login: req.body.login,
-        password: hashedPassword,
-        email: req.body.email
-    }, (err, user) => {
-        if(err) return res.status(500).send('There was problem registering the user.')
+router.post('/register', (req, res) => {
+    let error = '';
+    if (!req.body.login) {
+        error = 'Login is required!';
+    }
+    if (!req.body.password) {
+        error = 'Password is required!';
+    }
+    if (!req.body.email) {
+        error = 'Email is required!';
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(req.body.email)) {
+        error = 'Invalid email addres';
+    }
+    if (!error) {
+        const hashedPassword = bcrypt.hashSync(req.body.password, 8);
 
-        res.status(200).send({ auth: true });
-    });
+        User.create({
+            login: req.body.login,
+            password: hashedPassword,
+            email: req.body.email
+        }, (err, user) => {
+            if (err) return res.status(500).send('There was problem registering the user.')
+
+            res.status(200).send({
+                auth: true
+            });
+        });
+    } else {
+        res.status(400).send({
+            error
+        });
+    }
 });
 
-router.post('/login', (req,res) => {
-    User.findOne({ login: req.body.login }, (err,user) => {
-        if(err) return res.status(500).send('Error on the sever.');
-        if(!user) return res.status(404).send('No user found.');
+// Api endpoint for login user
+
+router.post('/login', (req, res) => {
+    User.findOne({
+        login: req.body.login
+    }, (err, user) => {
+        if (err) return res.status(500).send('Error on the sever.');
+        if (!user) return res.status(404).send('No user found.');
 
         const passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
-        if(!passwordIsValid) return res.status(401).send({ auth: false });
+        if (!passwordIsValid) return res.status(401).send({
+            auth: false
+        });
 
-        res.status(200).send({ auth: true });
+        res.status(200).send({
+            auth: true
+        });
     });
 });
 
--- todo/TodoController.js
+++ todo/TodoController.js
@@ -4,71 +4,97 @@ const mongoose = require('mongoose');
 const Post = require('../models/Post');
 
 const uri = 'mongodb://Luq:Haslo1@cluster0-shard-00-00-gw1sh.mongodb.net:27017,cluster0-shard-00-01-gw1sh.mongodb.net:27017,cluster0-shard-00-02-gw1sh.mongodb.net:27017/TodoList?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true';
-const db = mongoose.connect(uri, {useNewUrlParser: true}, () => {
+const db = mongoose.connect(uri, {
+    useNewUrlParser: true
+}, () => {
     console.log('connected');
-    
+
 });
 
 const router = express.Router();
 router.use(bodyParser.json());
-router.use(bodyParser.urlencoded({ extended: true }));
+router.use(bodyParser.urlencoded({
+    extended: true
+}));
 
-router.use((req,res,next) => {
+
+// Middlewere that console log connection to the server
+router.use((req, res, next) => {
     console.log('Connection');
     next();
 })
 
 router.route('/posts')
-    .post((req,res) =>{
-    const newTodo = req.body;
-    Post.create({
-        text: newTodo.text,
-        completed: newTodo.completed
-    }, (err, post) => {
-        if(err) res.status(500).send('There was a problem creating post.')
-        res.status(200).send('Post created.')
-        })
+    // Api endpoint for create a task
+    .post((req, res) => {
+        const newTodo = req.body;
+
+        let error = '';
+        if (!newTodo.text) {
+            error = 'Task is required!';
+        }
+        if (!error) {
+            Post.create({
+                text: newTodo.text,
+                completed: newTodo.completed
+            }, (err, post) => {
+                if (err) res.status(500).send('There was a problem creating post.')
+                res.status(200).send('Post created.')
+            })
+        } else {
+            res.status(400).send({ error });
+        }
     })
-        
-    .get((req,res) => {
-        Post.find((err,posts) => {
-            if(err) res.status(500).send('Error on the sever.');
-            if(!posts) return res.status(404).send('No posts found.');
+
+    // Api endpoint for get all tasks
+    .get((req, res) => {
+        Post.find((err, posts) => {
+            if (err) res.status(500).send('Error on the sever.');
+            if (!posts) return res.status(404).send('No posts found.');
             res.status(200).json(posts);
         })
     })
 
 
 router.route('/posts/:post_id')
-    .get((req,res) => {
-        Post.findById(req.params.post_id, (err,post) => {
-            if(err) res.status(500).send('Error on the sever.');
-            if(!post) return res.status(404).send('No post found.');
+    // Api endpoint for get post
+    .get((req, res) => {
+        Post.findById(req.params.post_id, (err, post) => {
+            if (err) res.status(500).send('Error on the sever.');
+            if (!post) return res.status(404).send('No post found.');
             res.status(200).json(post);
         })
     })
 
-    .put((req,res) => {
-        Post.findById(req.params.post_id, (err,post) => {
-            if(err) res.status(500).send('Error on the sever.');
-            if(!post) return res.status(404).send('No post found.');
-                post.completed = req.body.completed;
-                if(req.body.text){
-                    post.text = req.body.text;
-                }
+    // Api endpoint for edit post
+    .put((req, res) => {
+        Post.findById(req.params.post_id, (err, post) => {
+            if (err) res.status(500).send('Error on the sever.');
+            if (!post) return res.status(404).send('No post found.');
+            post.completed = req.body.completed;
+            if (req.body.text) {
+                post.text = req.body.text;
+            }
 
-                post.save(err => {
-                    if(err) res.status(500).send('Error on the sever.');
-                    res.status(200).json({message: 'Post updated'}); 
-                })
+            post.save(err => {
+                if (err) res.status(500).send('Error on the sever.');
+                res.status(200).json({
+                    message: 'Post updated'
+                });
+            })
         })
     })
 
-    .delete((req,res) => {
-        Post.deleteOne({_id: req.params.post_id}, (err,post) => {
-            if(err) res.status(500).send('Error on the sever.');
-            if(!post) return res.status(404).send('No post found.');
-            res.status(200).json({ message: 'Successfully deleted' })
+    // Api endpoint for delete post
+    .delete((req, res) => {
+        Post.deleteOne({
+            _id: req.params.post_id
+        }, (err, post) => {
+            if (err) res.status(500).send('Error on the sever.');
+            if (!post) return res.status(404).send('No post found.');
+            res.status(200).json({
+                message: 'Successfully deleted'
+            })
         })
     })
 
